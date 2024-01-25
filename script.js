446ccfa8da99cf675e921589fa78390d43726ac3
Gustavo Pimentel <gustavoafpimentel@gmail.com>
2024-01-25 20:40:01 -0300

    Função de pincel adicionada

--- script.js
+++ script.js
@@ -6,11 +6,36 @@ const tools = document.querySelectorAll('.button__tool');
 const sizeButtons = document.querySelector('.button__size');
 const buttonClear = document.querySelector('.button__clear');
 
-let brushSize = 10;
+let brushSize = 50;
+
+ctx.fillStyle = '#000';
+
+let isPainting = false;
+
+canvas.addEventListener('mousedown', ({clientX, clientY}) => {
+    draw(clientX, clientY);
+    isPainting = true;
+});
+
+canvas.addEventListener('mousemove', ({clientX, clientY}) => {
+    if(isPainting) {
+        draw(clientX, clientY);
+    }
+});
+
+canvas.addEventListener('mouseup', () => {
+    isPainting = false;
+});
 
 const draw = (x, y) => {
-    ctx.fillStyle = '#000';
-    ctx.fillRect(x, y, brushSize, brushSize);
-}
+    ctx.beginPath();
+    ctx.arc(
+        x - canvas.offsetLeft, 
+        y - canvas.offsetTop, 
+        brushSize / 2, 
+        0, 
+        2 * Math.PI);
+    ctx.fill();
+} 
 
 draw(100, 100); //teste
