6a2a804e744661cda2533bd5f01ba9c0e555bd10
Gustavo Pimentel <gustavoafpimentel@gmail.com>
2024-02-06 19:21:43 -0300

    Funcionalidade de Borracha adicionada

--- script.js
+++ script.js
@@ -10,18 +10,31 @@ let brushSize = 50;
 
 let isPainting = false;
 
+let activeTool = 'brush';
+
 inputColor.addEventListener('change', ({target}) => {
     ctx.fillStyle = target.value;
 });
 
 canvas.addEventListener('mousedown', ({clientX, clientY}) => {
-    draw(clientX, clientY);
     isPainting = true;
+
+    if(activeTool == 'brush') {
+        draw(clientX, clientY);
+    }
+    if(activeTool == 'rubber') {
+        erase(clientX, clientY);
+    }
 });
 
 canvas.addEventListener('mousemove', ({clientX, clientY}) => {
     if(isPainting) {
-        draw(clientX, clientY);
+        if(activeTool == 'brush') {
+            draw(clientX, clientY);
+        }
+        if(activeTool == 'rubber') {
+            erase(clientX, clientY);
+        }
     }
 });
 
@@ -30,6 +43,19 @@ canvas.addEventListener('mouseup', () => {
 });
 
 const draw = (x, y) => {
+    ctx.globalCompositeOperation = 'source-over';
+    ctx.beginPath();
+    ctx.arc(
+        x - canvas.offsetLeft, 
+        y - canvas.offsetTop, 
+        brushSize / 2, 
+        0, 
+        2 * Math.PI);
+    ctx.fill();
+}
+
+const erase = (x, y) => {
+    ctx.globalCompositeOperation = 'destination-out';
     ctx.beginPath();
     ctx.arc(
         x - canvas.offsetLeft, 
@@ -40,4 +66,15 @@ const draw = (x, y) => {
     ctx.fill();
 } 
 
-draw(100, 100); //teste
+const selectTool = ({target}) => {
+    const selectedTool = target.closest('button');
+    const action = selectedTool.getAttribute('data-action');
+    
+    if(action) {
+        activeTool = action;
+    }
+}
+
+tools.forEach((tool) => {
+    tool.addEventListener('click', selectTool);
+});
